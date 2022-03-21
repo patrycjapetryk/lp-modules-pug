839b3ab6d3df7cd45c447d02bebec41f145a2fc5
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2022-03-21 11:13:22 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+import '../scss/main.scss';
+
+// const videoTag = document.querySelector('.header__video--js');
+// const videoSourceTag = document.querySelector('.header__video--js source');
+// const videoSourceSrc = videoSourceTag.getAttribute('src');
+// const videoSourceDataSrc = videoSourceTag.getAttribute('data-src');
+// const tabletMediaQuery = 764;
+
+// function setVideoSource() {
+//   const windowWidth = window.innerWidth;
+//   const videoSource = videoSourceTag.getAttribute('src');
+
+//   if (windowWidth > tabletMediaQuery) {
+//     if (videoSource !== videoSourceDataSrc) {
+//       videoSourceTag.setAttribute('src', videoSourceDataSrc);
+//       videoTag.load();
+//     }
+//   } else {
+//     if (videoSource !== videoSourceSrc) {
+//       videoSourceTag.setAttribute('src', videoSourceSrc);
+//       videoTag.load();
+//     }
+//   }
+// }
+
+// window.addEventListener('load', setVideoSource);
+// window.addEventListener('resize', setVideoSource);
