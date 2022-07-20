6864d650baf34051ae3a1b84f0f2b01168942f64
Patrycja Petryk <patrycja.petryk@gmail.com>
2022-07-20 21:23:45 +0200

    JS rafactoring

--- src/js/background.js
+++ src/js/background.js
@@ -1,8 +1,8 @@
-const background = document.querySelector('.background--js');
+var background = document.querySelector('.background--js');
 
-const backgroundAnimation = () => {
-  const opacityValue = () => {
-    let opacity = 1 - 0.0001 * window.scrollY;
+var backgroundAnimation = function () {
+  var opacityValue = function () {
+    var opacity = 1 - 0.0001 * window.scrollY;
 
     if (opacity < 0.3) {
       opacity = 0.3;
