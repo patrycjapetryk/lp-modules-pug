e16ae68ed43764df65f47a854f4c574b1a8bd49b
Patrycja Petryk <patrycja.petryk@gmail.com>
2023-04-04 15:05:05 +0200

    Change main max width

--- src/js/background.js
+++ src/js/background.js
@@ -14,3 +14,22 @@ function backgroundAnimation() {
 document.addEventListener('scroll', backgroundAnimation);
 window.addEventListener('resize', backgroundAnimation);
 window.addEventListener('load', backgroundAnimation);
+
+//- handle background animation
+//- script.
+//-   (function () {
+//-   var background = document.querySelector('.background--js');
+//-   var backgroundAnimation = function () {
+//-     var opacityValue = function () {
+//-       var opacity = 1 - 0.0001 * window.scrollY;
+//-       if (opacity < 0.3) {
+//-         opacity = 0.3;
+//-       }
+//-       return opacity;
+//-     };
+//-     background.style.opacity = opacityValue();
+//-   };
+//-   backgroundAnimation();
+//-   document.addEventListener('scroll', backgroundAnimation);
+//-   window.addEventListener('resize', backgroundAnimation);
+//-   })();
